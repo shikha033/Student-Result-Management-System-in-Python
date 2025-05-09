7e2f74cd827afd42651675883e08fdc797eb8b27
shikha033 <shikhasingh242494@gmail.com>
2025-05-10 03:02:05 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,6 +8,12 @@ class RMS:
         self.root.title("Student Result Managment System")
         self.root.geometry("1350x700+0+0")
         self.root.config(bg="white")
+
+        # icons---
+        self.logo_dash = ImageTk.PhotoImage(file="images/logo_ch.png")
+
+        # title--  
+        title = Label(self.root, text="Student Result Managment System",compound=LEFT,image=self.logo_dash , bg = "#033054", fg="white", font=("goudy old style ", 20, "bold"))
         title.place(x=0, y=0, relwidth=1, height=50)
 if __name__ == "__main__":
     root = Tk()
