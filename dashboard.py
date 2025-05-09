28cfa68d4c82e73434e5af7c3c8d00861e2e88ac
shikha033 <shikhasingh242494@gmail.com>
2025-05-10 02:37:58 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,8 @@ class RMS:
         self.root.geometry("1350x700+0+0")
         self.root.config(bg="white")
              
-        title-Label(self.root, text="STUDENT RESULT MANAGEMENT SYSTEM", bd=10, relief=GROOVE, font=("times new roman", 30, "bold)
+        title = Label(self.root, text="Student Result Managment System", bg = "#033054", fg="white", font=("goudy old style ", 20, "bold"))
+        title.place(x=0, y=0, relwidth=1, height=50)
 if __name__ == "__main__":
     root = Tk()
     obj = RMS(root)
