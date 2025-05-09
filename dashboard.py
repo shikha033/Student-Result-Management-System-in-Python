90843146b9a250f26fdf9c14069d0c506c449dd0
shikha033 <shikhasingh242494@gmail.com>
2025-05-10 02:59:48 +0530

    updated

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,13 @@
 from tkinter import *
 
+from PIL import Image, ImageTk  
+
 class RMS:
     def __init__(self, root):
         self.root = root
         self.root.title("Student Result Managment System")
         self.root.geometry("1350x700+0+0")
         self.root.config(bg="white")
-             
-        title = Label(self.root, text="Student Result Managment System", bg = "#033054", fg="white", font=("goudy old style ", 20, "bold"))
         title.place(x=0, y=0, relwidth=1, height=50)
 if __name__ == "__main__":
     root = Tk()
