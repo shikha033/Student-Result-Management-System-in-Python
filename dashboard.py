2d9af7a4079ad4608cb08538ba6ff47fd04ff51d
shikha033 <shikhasingh242494@gmail.com>
2025-05-11 02:49:14 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,8 +13,17 @@ class RMS:
         self.logo_dash = ImageTk.PhotoImage(file="images/logo_ch.png")
 
         # title--  
-        title = Label(self.root, text="Student Result Managment System",compound=LEFT,image=self.logo_dash , bg = "#033054", fg="white", font=("goudy old style ", 20, "bold"))
+        title = Label(self.root, text="Student Result Managment System",padx=10,compound=LEFT,image=self.logo_dash , bg = "#033054", fg="white", font=("goudy old style ", 20, "bold"))
         title.place(x=0, y=0, relwidth=1, height=50)
+
+
+        # menu---
+        m_frame=LabelFrame(self.root, text="Menu",font=("times new roman ",15) ,bg="white")
+        m_frame.place(x=10, y=70, width=1250, height=80)
+        # buttons---
+        btn_course = Button(m_frame, text="COURSES", width=20, font=("goudy old style", 15,"bold"), bg="#0b5377", fg="white")
+        btn_course.place(x=20, y=5,width=200,height=40)
+
 if __name__ == "__main__":
     root = Tk()
     obj = RMS(root)
