c823f9b18a485ae3c01d998ea1424e66e7f5aa69
shikha033 <shikhasingh242494@gmail.com>
2025-07-17 23:51:04 +0530

    Update course.py

--- course.py
+++ course.py
@@ -21,6 +21,11 @@ class CourseClass:
      
 
 
+        #===============widgets==============
+        lbl_courseName=Label(self.root, text="Course Name", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=60)
+        lbl_duration=Label(self.root, text="Duration", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=100)
+        lbl_charges=Label(self.root, text="Charges", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=140)
+        lbl_description=Label(self.root, text="Description", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=180)
 if __name__ == "__main__":
 
     root = Tk()
