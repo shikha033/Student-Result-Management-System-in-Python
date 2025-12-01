687e3b34454f45d9b9710d9feb3206b5017f43a2
shikha033 <shikhasingh242494@gmail.com>
2025-12-02 01:17:29 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ import sqlite3
 def create_db():
     conn = sqlite3.connect('rms.db')
     c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS course(cid INTEGER PRIMARY KEY AUTOINCREMENT, name text, charges text, description text)")
+    c.execute("CREATE TABLE IF NOT EXISTS course(cid INTEGER PRIMARY KEY AUTOINCREMENT, name text, duration text, charges text, description text)")
 
     conn.commit()
     conn.close()
