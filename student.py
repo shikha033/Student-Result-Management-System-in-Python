3ce77c780f1477a3a48f7bd89f0b6d6861d704fd
shikha033 <shikhasingh242494@gmail.com>
2025-12-27 23:24:18 +0530

    Update student.py

--- student.py
+++ student.py
@@ -51,7 +51,7 @@ class StudentClass:
         txt_name=Entry(self.root, textvariable=self.var_name, font=("goudy old style", 15, 'bold'), bg='lightyellow').place(x=150, y=100, width=200)
 
         txt_email=Entry(self.root, textvariable=self.var_email, font=("goudy old style", 15, 'bold'),state='readonly',justify=CENTER).place(x=150, y=140, width=200)
-        self.txt_gender=ttk.Combobox(self.root, textvariable=self.var_gender, values=("Select","Male", "Female", "Other"),font=("goudy old style", 15, 'bold'))
+        self.txt_gender=ttk.Combobox(self.root, textvariable=self.var_gender, values=("Select","Male", "Female", "Other"),justify=CENTER,font=("goudy old style", 15, 'bold'))
         self.txt_gender.place(x=150, y=180, width=200)
         self.txt_gender.current(0)
         self.txt_address=Text(self.root, font=("goudy old style", 15, 'bold'), bg='lightyellow')
