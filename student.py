50d99125aed0b3c8b1064454f7fa483c9ce5c51b
shikha033 <shikhasingh242494@gmail.com>
2026-01-22 22:36:53 +0530

    Update student.py

--- student.py
+++ student.py
@@ -39,6 +39,9 @@ class StudentClass:
         lbl_gender=Label(self.root, text="Gender", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=180)
         lbl_state=Label(self.root, text="State", font=("goudy old style", 15, 'bold'), bg='white').place(x=10, y=220)
         
+        txt_state=Entry(self.root, textvariable=self.var_state, font=("goudy old style", 15, 'bold'), bg='lightyellow').place(x=150, y=220, width=150)
+        lbl_city=Label(self.root, text="City", font=("goudy old style", 15, 'bold'), bg='white').place(x=300, y=220)
+        txt_city=Entry(self.root, textvariable=self.var_city, font=("goudy old style", 15, 'bold'), bg='lightyellow').place(x=370, y=220, width=100)
 
 
 
