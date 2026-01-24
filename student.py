1596f01e97007408c64618bf8f9132a3b6d65b51
shikha033 <shikhasingh242494@gmail.com>
2026-01-24 22:48:58 +0530

    Update student.py

--- student.py
+++ student.py
@@ -79,7 +79,7 @@ class StudentClass:
         txt_admisssion=Entry(self.root, textvariable=self.var_a_date, font=("goudy old style", 15, 'bold'),state='readonly',justify=CENTER).place(x=480, y=140, width=200)
         self.txt_course=ttk.Combobox(self.root, textvariable=self.var_course, values=self.course_list,justify=CENTER,font=("goudy old style", 15, 'bold'))
         self.txt_course.place(x=480, y=180, width=200)
-        self.txt_course.set("Empty")
+        self.txt_course.set("Select the course")
        #=================text address=================
 
 
