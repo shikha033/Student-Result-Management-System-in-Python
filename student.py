02c56d5f6c0b0b964ed3265860c3be96ef5bc6ec
shikha033 <shikhasingh242494@gmail.com>
2026-01-25 23:08:24 +0530

    Update student.py

--- student.py
+++ student.py
@@ -118,8 +118,6 @@ class StudentClass:
         scrollx.config(command=self.CourseTable.xview)
         scrolly.config(command=self.CourseTable.yview)
 
-        self.CourseTable.heading("duration", text="Duration")
-        self.CourseTable.heading("charges", text="Charges")
         self.CourseTable.heading("description", text="Description")
         self.CourseTable["show"]="headings" # otherwise one extra empty column will be there
         self.CourseTable.column("cid", width=100)
